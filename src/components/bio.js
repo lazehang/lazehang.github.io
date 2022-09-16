eb84d21fe7533f057cd77f902ef9237971fc326a
Mangal Hang Limbu <lazehang@gmail.com>
2022-09-16 18:36:28 +0800

    update

--- src/components/bio.js
+++ src/components/bio.js
@@ -31,13 +31,31 @@ const Bio = () => {
 
     return (
         <div className="grid md:grid-cols-2 gap-10 items-center justify-center md:py-10">
-            <div className="md:order-2">
+            <div className="md:order-2 relative grid grid-cols-8 grid-rows-6">
                 <StaticImage
                     src="../images/profile-pic.jpg"
                     width={700}
                     height={700}
                     alt="Profile picture"
-                    className="rounded-[20px]"
+                    className="rounded-[20px] col-start-2 col-span-6 row-start-2 row-span-4"
+                />
+                <StaticImage
+                    src="../images/portfolio/spotlight.jpg"
+                    width={700}
+                    alt="spotlight studio hk"
+                    className="rounded-md col-start-1 col-span-2 row-start-1 row-span-2"
+                />
+                <StaticImage
+                    src="../images/portfolio/getbarcode.jpg"
+                    width={700}
+                    alt="getbarcode"
+                    className="rounded-md col-start-6 col-end-9 row-start-3"
+                />
+                <StaticImage
+                    src="../images/portfolio/dibyadristy.jpg"
+                    width={700}
+                    alt="Dibya dristy"
+                    className="rounded-md col-start-4 col-span-5 row-start-5 row-span-2 mr-4"
                 />
             </div>
             {author?.name && (
@@ -78,10 +96,10 @@ const Bio = () => {
                         .
                     </p>
                     <a
-                        href="/resume.pdf"
+                        href="/Resume-Mangal-Limbu.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-block p-3 border border-black rounded text-black font-bold"
+                        className="inline-block p-3 border border-black rounded text-black font-semibold"
                     >
                         Checkout My Resume
                     </a>
