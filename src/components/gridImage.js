d43698b650a9a7932326f9a62358d2754f3c7b11
Mangal Hang Limbu <lazehang@gmail.com>
2022-09-19 14:46:22 +0800

    update image border radius

--- src/components/gridImage.js
+++ src/components/gridImage.js
@@ -9,18 +9,20 @@ export default function gridImage({ className = '' }) {
                 width={700}
                 height={700}
                 alt="Profile picture"
-                className="rounded-[20px] col-start-2 col-span-6 row-start-2 row-span-4"
+                className="col-start-2 col-span-6 row-start-2 row-span-4"
+                imgClassName="rounded-[20px]"
             />
             <a
                 href="https://www.spotlightstudiohk.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="rounded-md col-start-1 col-span-2 row-start-1 row-span-2 overflow-hidden"
+                className="col-start-1 col-span-2 row-start-1 row-span-2 overflow-hidden"
             >
                 <StaticImage
                     src="../images/portfolio/spotlight.jpg"
                     width={700}
                     alt="spotlight studio hk"
+                    imgClassName="rounded-md"
                 />
             </a>
             <a
@@ -33,13 +35,15 @@ export default function gridImage({ className = '' }) {
                     src="../images/portfolio/getbarcode.jpg"
                     width={700}
                     alt="getbarcode"
+                    imgClassName="rounded-md"
                 />
             </a>
             <StaticImage
                 src="../images/portfolio/dibyadristy.jpg"
                 width={700}
                 alt="Dibya dristy"
-                className="rounded-md col-start-4 col-span-5 row-start-5 row-span-2 mr-4"
+                className="col-start-4 col-span-5 row-start-5 row-span-2 mr-4"
+                imgClassName="rounded-md"
             />
         </div>
     )
